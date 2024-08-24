0db9c85a545c638f94b6bf2c5888272bbe77da94
omid <omiidnk02@gmail.com>
2024-08-24 23:52:31 +0330

    Config

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+
+      boxShadow: {
+        normal: "0px 1px 10px rgba(0, 0, 0, 0.05)",
+      },
+
+      borderRadius: {
+        "4xl": "2rem",
+      },
+
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemibold: "Dana Demibold",
+        MorabbaLight: "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
+    },
+  },
+  plugins: [],
+};
